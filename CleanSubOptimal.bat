ef96e9fcec517a5741b2bd66c8f8d65663e5b3db
gkartsiotis <gkartsiotis@gmail.com>
2022-08-24 14:21:41 +0300

    Delete CleanSubOptimal.bat

--- CleanSubOptimal.bat
+++
@@ -1,8 +0,0 @@
-del *.txt
-del *.asv
-del *.fig
-del *.mexw64
-del *.mat
-del *.avi
-RD /S /Q C:\FALCON.m.v1.24.2002191427\falcon\SetupA_Final\fm_models
-RD /S /Q C:\FALCON.m.v1.24.2002191427\falcon\SetupA_Final\fm_constraints
